aaea876833e4f26ec3b33b6139141ce43ea014ed
Shashank Ellareddy <66846202+listentothefrog@users.noreply.github.com>
2021-04-10 12:41:46 -0400

    makes the bot come online

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello my name is Daily Bot!")
+import discord
+import os
+import requests
+import json
+import random
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  msg = message.content
+client.run(os.getenv('DISCORD_API_KEY'))
