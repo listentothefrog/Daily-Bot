7a4b0af6b0f2ccb8f378273c9363d5a2a2933d88
Shashank Ellareddy <66846202+listentothefrog@users.noreply.github.com>
2021-07-16 18:58:06 -0400

    calls motivate function

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import discord
 import os
 import requests
 import json
-import random
-from get_news import *
+from get_news import get_latest_news
+from keep_alive import keep_alive
+from motivate import get_quote
 
 client = discord.Client()
 
@@ -15,8 +16,13 @@ async def on_ready():
 async def on_message(message):
   if message.author == client.user:
     return
+  if message.content.startswith('$inspire'):
+    quote = get_quote()
+    await message.channel.send(quote)
+
   if message.content.startswith('$daily-news'):
     news = get_latest_news()
     await message.channel.send(news)
-  
+
+keep_alive()
 client.run(os.getenv('DISCORD_API_KEY'))
